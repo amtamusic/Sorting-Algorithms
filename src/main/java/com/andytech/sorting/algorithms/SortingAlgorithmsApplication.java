76d88d99a2c46b516288e41d2f080ae2c5850b2d
Andres Toledo <amtamusic@users.noreply.github.com>
2022-09-22 00:05:33 -0400

    Initial commit

---
+++ src/main/java/com/andytech/sorting/algorithms/SortingAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.andytech.sorting.algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andytech/sorting/algorithms/SortingAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andytech.sorting.algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
