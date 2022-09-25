f5629b2b408ee234b5584ed03f39da09e40e7f6c
Andres Toledo <amtamusic@users.noreply.github.com>
2022-09-25 02:09:17 -0400

    Added visualization for different sorting algorithms
    -Note UI hangs displaying more then approximately 100 steps.
    -Home page added allowing user to input csv array(validations pending), and order type.

--- src/main/java/com/andytech/sorting/algorithms/controllers/GraphicSortingController.java
+++ src/main/java/com/andytech/sorting/algorithms/controllers/GraphicSortingController.java
@@ -1,49 +1,83 @@
 package com.andytech.sorting.algorithms.controllers;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Map;
-import java.util.TreeMap;
-
+import com.andytech.sorting.algorithms.pojos.inputs.ArrayString;
 import com.andytech.sorting.algorithms.pojos.response.SortingResponse;
 import com.andytech.sorting.algorithms.services.SortingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class GraphicSortingController {
+
+    Logger logger = LoggerFactory.getLogger(GraphicSortingController.class);
 
     @Autowired
     private SortingService sortingService;
 
-
-    @GetMapping("/graph/selection")
-    public String graphSelectionSort(Model model) {
+    @GetMapping("/")
+    public String home(Model model)
+    {
+        ArrayString arrayString = new ArrayString();
+        model.addAttribute("arrayString",arrayString);
+        return "home";
+    }
+    @PostMapping("/graph/selection")
+    public String graphSelectionSort(Model model,@ModelAttribute(value = "arrayString") ArrayString arrayString) {
+        logger.info("Input Array: "+ arrayString.getArray());
+        logger.info("Is Ascending: "+ arrayString.isAscending());
         String algorithm = "Selection Sort";
-        SortingResponse sortingResponse = sortingService.selectionSort(new ArrayList<>(Arrays.asList(5, 3, 2, 4, 1)), "asc", true);
-        Map<String, Integer> graphData = new TreeMap<>();
-        for(int i =0 ; i < sortingResponse.getItemsToSort().size(); i++){
-            graphData.put(i+"", (Integer) sortingResponse.getSorted().get(i));
-        }
+        SortingResponse sortingResponse = sortingService.selectionSort(arrayString.getArrayInt(), arrayString.isAscending()?"asc":"desc", true);
         model.addAttribute("algorithm", algorithm);
-        model.addAttribute("chartData", graphData);
         model.addAttribute("steps", sortingResponse.getSteps());
         model.addAttribute("step",0);
+        model.addAttribute("sorted",sortingResponse.getSorted().toString());
         return "google-charts";
     }
 
-    @GetMapping("/graph/bubble")
-    public String getBubbleSort(Model model) {
-        String algorithm = "Selection Sort";
-        SortingResponse sortingResponse = sortingService.selectionSort(new ArrayList<>(Arrays.asList(5, 3, 2, 4, 1)), "asc", true);
-        Map<String, Integer> graphData = new TreeMap<>();
-        for(int i =0 ; i < sortingResponse.getItemsToSort().size(); i++){
-            graphData.put(i+"", (Integer) sortingResponse.getSorted().get(i));
-        }
+    @PostMapping("/graph/bubble")
+    public String getBubbleSort(Model model,@ModelAttribute(value = "arrayString") ArrayString arrayString) {
+        logger.info("Input Array: "+ arrayString.getArray());
+        logger.info("Is Ascending: "+ arrayString.isAscending());
+        String algorithm = "Bubble Sort";
+        SortingResponse sortingResponse = sortingService.bubbleSort(arrayString.getArrayInt(), arrayString.isAscending()?"asc":"desc", true);
         model.addAttribute("algorithm", algorithm);
-        model.addAttribute("chartData", graphData);
         model.addAttribute("steps", sortingResponse.getSteps());
         model.addAttribute("step",0);
+        model.addAttribute("sorted",sortingResponse.getSorted().toString());
+        return "google-charts";
+    }
+
+    @PostMapping("/graph/insertion")
+    public String getInsertionSort(Model model,@ModelAttribute(value = "arrayString") ArrayString arrayString) {
+        logger.info("Input Array: "+ arrayString.getArray());
+        logger.info("Is Ascending: "+ arrayString.isAscending());
+        String algorithm = "Insertion Sort";
+        SortingResponse sortingResponse = sortingService.insertionSort(arrayString.getArrayInt(), arrayString.isAscending()?"asc":"desc", true);
+        model.addAttribute("algorithm", algorithm);
+        model.addAttribute("steps", sortingResponse.getSteps());
+        model.addAttribute("step",0);
+        model.addAttribute("sorted",sortingResponse.getSorted().toString());
+        return "google-charts";
+    }
+
+    @PostMapping("/graph/merge")
+    public String getMergeSort(Model model,@ModelAttribute(value = "arrayString") ArrayString arrayString) {
+        logger.info("Input Array: "+ arrayString.getArray());
+        logger.info("Is Ascending: "+ arrayString.isAscending());
+        String algorithm = "Merge Sort";
+        SortingResponse sortingResponse = sortingService.mergeSort(arrayString.getArrayInt(), arrayString.isAscending()?"asc":"desc", true);
+        model.addAttribute("algorithm", algorithm);
+        model.addAttribute("steps", sortingResponse.getSteps());
+        model.addAttribute("step",0);
+        model.addAttribute("sorted",sortingResponse.getSorted().toString());
         return "google-charts";
     }
 }
